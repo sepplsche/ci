c4e0f7ac4bc8edd1ba3f96bf4fa76fb322549a4a
seppl <seppl>
2016-09-24 08:56:39 +0200

    echo added und icanhazip verwenden

--- ip.sh
+++ ip.sh
@@ -2,18 +2,19 @@
 echo checking for updated ip...
 
 cd c:/xampp/htdocs/ci
-
+git pull -p
 LASTIP=$(<ip.txt)
 
 while [ true ]; do
 
-	NEWIP=$(curl -s http://myip.dnsdynamic.org/)
+	NEWIP=$(curl -s https://icanhazip.com/)
 
 	if [ "$LASTIP" != "$NEWIP" ]; then
 		echo $NEWIP > ip.txt
 		git commit -a -m "ip updated"
 		git push
 		LASTIP=$NEWIP
+		echo ip updated
 	fi
 
 	sleep 5m
